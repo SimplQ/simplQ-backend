f51168361fe7a492443935fdc61f7786f25cd755
daltonfury42 <daltonfury42@disroot.org>
2020-05-17 17:03:45 +0530

    Minor fixes

--- complete/src/main/java/com/example/restservice/controller/QueueController.java
+++ complete/src/main/java/com/example/restservice/controller/QueueController.java
@@ -2,11 +2,9 @@ package com.example.restservice.controller;
 
 import com.example.restservice.model.CreateQueueRequest;
 import com.example.restservice.model.CreateQueueResponse;
-import com.example.restservice.model.Queue;
 import com.example.restservice.model.QueueDetailsResponse;
 import com.example.restservice.service.QueueService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,13 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QueueController {
 
-  private Queue queue;
-
   @Autowired private QueueService queueService;
 
   @GetMapping(value = "/v1/queue/{queueId}")
-  public ResponseEntity<QueueDetailsResponse> getQueueDetails(@PathVariable String queueId) {
-    return ResponseEntity.ok(queueService.fetchQueueData(queueId));
+  public QueueDetailsResponse getQueueDetails(@PathVariable String queueId) {
+    return queueService.fetchQueueData(queueId);
   }
 
   @PostMapping(path = "v1/queue")
