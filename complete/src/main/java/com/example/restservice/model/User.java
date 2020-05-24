08eafa8662df6f5a7113ead5b6becdbe7de83fa5
daltonfury42 <daltonfury42@disroot.org>
2020-05-24 15:28:26 +0530

    RDS

--- complete/src/main/java/com/example/restservice/model/User.java
+++ complete/src/main/java/com/example/restservice/model/User.java
@@ -4,9 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Table(name = "QueueUser") // As user is a reserved keyword in PostgreSQL
 public class User {
   public enum UserStatus {
     WAITING,
