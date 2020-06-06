a9e762e395b8f7b53a74bf2fd5478a71ee4be962
sajmalyousef <yousefsajmal1@gmail.com>
2020-06-06 13:31:42 +0530

    move to bean config for datasource

--- complete/src/main/java/com/example/restservice/dao/QueueRepository.java
+++ complete/src/main/java/com/example/restservice/dao/QueueRepository.java
@@ -1,7 +1,10 @@
 package com.example.restservice.dao;
 
+import com.example.restservice.RestServiceApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface QueueRepository extends CrudRepository<Queue, String> {}
