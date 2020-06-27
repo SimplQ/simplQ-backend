dd2cd599512f5baa5a55e4993d844539833fb4a2
daltonfury42 <daltonfury42@users.noreply.github.com>
2020-06-27 16:41:56 +0530

    Throw unauthorized on missing auth header (#35)

--- complete/src/main/java/com/example/restservice/controller/advices/SQExcpetionHandlerAdvice.java
+++ complete/src/main/java/com/example/restservice/controller/advices/SQExcpetionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.restservice.controller.advices;
 
+import com.example.restservice.exceptions.SQAccessDeniedException;
 import com.example.restservice.exceptions.SQInvalidRequestException;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.http.HttpHeaders;
@@ -19,9 +20,23 @@ public class SQExcpetionHandlerAdvice extends ResponseEntityExceptionHandler {
     var response =
         new DefaultErrorAttributes()
             .getErrorAttributes(
-                request, false /* includeStackTrace */); // TODO hide stacktrace on prod
+                request, false /* includeStackTrace */);
     var httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
     response.put("reasonCode", ex.getReasonCode());
+    response.put("status", httpStatus.value());
+    response.put("error", httpStatus.getReasonPhrase());
+    response.put("message", ex.getMessage());
+    return handleExceptionInternal(ex, response, new HttpHeaders(), httpStatus, request);
+  }
+
+  @ExceptionHandler(value = {SQAccessDeniedException.class})
+  protected ResponseEntity<Object> handlerIAccessDenied(
+      SQAccessDeniedException ex, WebRequest request) {
+    var response =
+        new DefaultErrorAttributes()
+            .getErrorAttributes(
+                request, false /* includeStackTrace */);
+    var httpStatus = HttpStatus.UNAUTHORIZED;
     response.put("status", httpStatus.value());
     response.put("error", httpStatus.getReasonPhrase());
     response.put("message", ex.getMessage());
