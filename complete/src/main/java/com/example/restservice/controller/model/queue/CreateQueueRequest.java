41650614f37b79f30f6bd74bffd641285017be10
NIRMAL K V <nirmalkvaduva@gmail.com>
2020-07-05 02:10:46 +0530

    Validation for Queue names (#57)

--- complete/src/main/java/com/example/restservice/controller/model/queue/CreateQueueRequest.java
+++ complete/src/main/java/com/example/restservice/controller/model/queue/CreateQueueRequest.java
@@ -2,8 +2,11 @@ package com.example.restservice.controller.model.queue;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 public class CreateQueueRequest {
 
+  @Pattern(regexp = "^[a-zA-Z0-9-]+$")
   String queueName;
 }
