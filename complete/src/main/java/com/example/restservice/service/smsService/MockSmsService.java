79132052b2466e31995c21d0cdff04f7e858015e
pavan541cs <pavankrishna541cs@gmail.com>
2020-08-01 22:54:51 +0530

    Using lombok logger and some comment fixes

--- complete/src/main/java/com/example/restservice/service/smsService/MockSmsService.java
+++ complete/src/main/java/com/example/restservice/service/smsService/MockSmsService.java
@@ -1,18 +1,16 @@
 package com.example.restservice.service.smsService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile("!prod")
 public class MockSmsService implements SmsService {
-    private final Logger LOGGER = LoggerFactory.getLogger(MockSmsService.class);
-
     @Override
     public String sendSMS(String contactNumber, String queueName) {
-        LOGGER.warn("Mock SMS Sent");
+        log.warn("Mock SMS Sent");
         return "SUCCESS";
     }
 }
