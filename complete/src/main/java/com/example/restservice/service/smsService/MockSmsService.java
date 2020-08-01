b416785445dae9a377bdc46a41ad1704d6b606a1
pavan541cs <pavankrishna541cs@gmail.com>
2020-08-01 16:57:05 +0530

    Moved common variables to common file

--- complete/src/main/java/com/example/restservice/service/smsService/MockSmsService.java
+++ complete/src/main/java/com/example/restservice/service/smsService/MockSmsService.java
@@ -1,13 +1,18 @@
 package com.example.restservice.service.smsService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("!prod")
 public class MockSmsService implements SmsService {
+    private final Logger LOGGER = LoggerFactory.getLogger(MockSmsService.class);
+
     @Override
     public String sendSMS(String contactNumber, String queueName) {
+        LOGGER.warn("Mock SMS Sent");
         return "SUCCESS";
     }
 }
