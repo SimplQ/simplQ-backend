1d8359898dd4b31c77dca638305ada0cff993d2a
pavan541cs <pavankrishna541cs@gmail.com>
2020-08-02 12:35:12 +0530

    Changes to manage env variables using spring profiles (#68)

---
+++ complete/src/main/java/com/example/restservice/service/smsService/MockSmsService.java
@@ -0,0 +1,16 @@
+package com.example.restservice.service.smsService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("!prod")
+public class MockSmsService implements SmsService {
+    @Override
+    public String sendSMS(String contactNumber, String queueName) {
+        log.warn("Mock SMS Sent");
+        return "SUCCESS";
+    }
+}
