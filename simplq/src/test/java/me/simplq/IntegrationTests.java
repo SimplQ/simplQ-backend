4b84740a8f43e6f341053b1e6b5456120ad71db9
dhimathu <dhimathu@cisco.com>
2020-10-04 13:30:09 +0530

    add e2e integration test

--- simplq/src/test/java/me/simplq/IntegrationTests.java
+++ simplq/src/test/java/me/simplq/IntegrationTests.java
@@ -3,11 +3,14 @@ package me.simplq;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.simplq.config.TestConfig;
 import me.simplq.controller.model.queue.CreateQueueResponse;
+import me.simplq.controller.model.queue.QueueDetailsResponse;
+import me.simplq.controller.model.token.TokenDetailResponse;
 import me.simplq.dao.QueueRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -16,6 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,31 +36,101 @@ class IntegrationTests {
 
   @Autowired private ObjectMapper objectMapper;
 
-  private String queueId;
+  @Value("${auth.token}")
+  private String authToken;
 
   @Test
-  void createQueueTest() throws Exception {
+  void endToEndScenarioTest() throws Exception {
 
+    // POST queue
     String createQueueRequest = "{ \"queueName\": \"Queue2222\" }";
 
-    MvcResult result =
+    MvcResult createQueueResult =
         mockMvc
             .perform(
                 post("/v1/queue", 42L)
                     .contentType("application/json")
-                    .content(createQueueRequest)
-                    .header(
-                        "Authorization",
-                        "Bearer eyJraWQiOiJMOG1UNExrbm9Sb2FLaWsxeTlURE1IZlQ0RXdUTk5YY2taMlVsK2NreUZZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1YTFiZWZiYS1lODM1LTRjYTAtYTNiYi05ODNkMjY2M2UwNDAiLCJldmVudF9pZCI6ImM4Yjc5YzBkLTJmNGMtNGMxMi05MjJiLTlhNmM1ZjcxMTU0NyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1OTMyNzA4NzIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMV9pUWRsNUFWckEiLCJleHAiOjE1OTMyNzQ0NzIsImlhdCI6MTU5MzI3MDg3MiwianRpIjoiNjM2YjY3NDYtYTk2OC00ZTcxLWE3ZmEtNGU4NGEwYzU0NDdlIiwiY2xpZW50X2lkIjoiMWY3bzZvNHZoaGhucWpiYjdob21hbHFtdnIiLCJ1c2VybmFtZSI6IjVjOWNkNDI2LWY3ZTMtNDVjYy1hYmUxLWI4NDc0YzdlMTExNSJ9.DOETlW-LnGwpI73XbDOtfpe9jlAhjWAVmHPdVmntUqu2SxI2Y26LIX2vLg0Z2AAltaut2Xxrl2W-yMi4gtbXECACjQeZyfwwx62hwR56pX0S-oAa_JtlsHlmzdvYaGDyGE5CDI3skSbFy_eSQM6xueogJGxRbGBh6W_C9c5lZZNKfiYn8DTL89seSiiXOU1VigsHggcsniBcFz0JYq4SHH17KjGl4RhYDrDC6aMVV3wM1ZiJmZ8MFUYpVfDLBlDCVJSzrZ93vDOsTTLg_xHDFq84J3mVoK5IJ9yFC5oLBWtyT-9h-IZe12Gr1BZpu8MlOEuAOppe0a-3ZI4uHb2y_Q"))
+                    .header("Authorization", authToken)
+                    .content(createQueueRequest))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andReturn();
 
-    String response = result.getResponse().getContentAsString();
-    CreateQueueResponse apiResponse = objectMapper.readValue(response, CreateQueueResponse.class);
-    Assertions.assertEquals("Queue2222", apiResponse.getQueueName());
-    //store this queue id for further tests
-    queueId = apiResponse.getQueueId();
+    String response = createQueueResult.getResponse().getContentAsString();
+    CreateQueueResponse createQueueResponse =
+        objectMapper.readValue(response, CreateQueueResponse.class);
+    Assertions.assertEquals("Queue2222", createQueueResponse.getQueueName());
 
+    // GET queue by id
+    MvcResult getQueueResult =
+        mockMvc
+            .perform(
+                get("/v1/queue/" + createQueueResponse.getQueueId())
+                    .header("Authorization", authToken))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+    QueueDetailsResponse queueDetailsResponse =
+        objectMapper.readValue(
+            getQueueResult.getResponse().getContentAsString(), QueueDetailsResponse.class);
+    Assertions.assertEquals(queueDetailsResponse.getQueueId(), createQueueResponse.getQueueId());
+    Assertions.assertEquals(
+        queueDetailsResponse.getQueueName(), createQueueResponse.getQueueName());
+
+    // POST token
+    String createTokenRequest =
+        "{ \"contactNumber\": \"999999999\","
+            + "\"name\": \"user name\","
+            + "\"queueId\": \""
+            + createQueueResponse.getQueueId()
+            + "\"}";
+
+    MvcResult createTokenResult =
+        mockMvc
+            .perform(
+                post("/v1/token", 42L)
+                    .contentType("application/json")
+                    .header("Authorization", authToken)
+                    .content(createTokenRequest))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+    TokenDetailResponse createTokenResponse =
+        objectMapper.readValue(
+            createTokenResult.getResponse().getContentAsString(), TokenDetailResponse.class);
+    Assertions.assertEquals(createTokenResponse.getQueueId(), createQueueResponse.getQueueId());
+    Assertions.assertEquals(createTokenResponse.getQueueName(), createQueueResponse.getQueueName());
+
+    // GET token by id
+    MvcResult getTokenResult =
+        mockMvc
+            .perform(
+                get("/v1/queue/" + createTokenResponse.getQueueId())
+                    .header("Authorization", authToken))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+    TokenDetailResponse tokenDetailsResponse =
+        objectMapper.readValue(
+            getTokenResult.getResponse().getContentAsString(), TokenDetailResponse.class);
+    Assertions.assertEquals(tokenDetailsResponse.getQueueId(), createQueueResponse.getQueueId());
+    Assertions.assertEquals(
+        tokenDetailsResponse.getQueueName(), createQueueResponse.getQueueName());
+
+    // GET myQueues
+    mockMvc
+        .perform(get("/v1/queues").header("Authorization", authToken))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // GET myTokens
+    mockMvc
+        .perform(get("/v1/tokens").header("Authorization", authToken))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
   }
 }
--- simplq/src/test/java/me/simplq/config/TestConfig.java
+++ simplq/src/test/java/me/simplq/config/TestConfig.java
@@ -1,36 +1,12 @@
 package me.simplq.config;
-import org.springframework.boot.test.context.TestConfiguration;
-import me.simplq.dao.QueueRepository;
-import me.simplq.service.QueueService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.beans.factory.annotation.Autowired;
-import me.simplq.config.DataSourceConfig;
 
+import me.simplq.service.QueueService;
+import me.simplq.service.TokenService;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
 
 @TestConfiguration
-@Import({QueueRepository.class,DataSourceConfig.class})
-public class TestConfig {
-
-    @Autowired
-    private QueueRepository queueRepository;
-
-    @Bean
-    public QueueService queueService(){
-        return new QueueService(queueRepository,null);
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean configureEntityManagerFactory() {
-        public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-            LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-            entityManagerFactoryBean.setDataSource(configureDataSource());
-            entityManagerFactoryBean.setPackagesToScan("${package}");
-            entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-            Properties jpaProperties = new Properties();
-            jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, dialect);
-            jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, hbm2ddlAuto);
-            entityManagerFactoryBean.setJpaProperties(jpaProperties);
-            return entityManagerFactoryBean;
-        }
-}
+@EnableJpaRepositories("me.simplq.dao")
+@ContextConfiguration(classes = {QueueService.class, TokenService.class})
+public class TestConfig {}
