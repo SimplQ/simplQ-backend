2e916882174db8fe87afb584c64707bd2b580425
daltonfury42 <daltonfury42@disroot.org>
2020-10-26 22:39:30 +0530

    Deleted queues should not come in my queues

--- simplq/src/main/java/me/simplq/controller/model/queue/MyQueuesResponse.java
+++ simplq/src/main/java/me/simplq/controller/model/queue/MyQueuesResponse.java
@@ -1,11 +1,18 @@
 package me.simplq.controller.model.queue;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class MyQueuesResponse {
+
+  public MyQueuesResponse() {
+    this.queues = new ArrayList<>();
+  }
 
   @Data
   public static class Queue {
