af5f30823ba5e2cd0d56cd856f22183b374f469f
Dalton Fury <daltonfury42@gmail.com>
2021-01-01 20:14:37 +0530

    Auth Filter Code Cleanup

--- simplq/src/main/java/me/simplq/controller/advices/AuthenticationFilter.java
+++ simplq/src/main/java/me/simplq/controller/advices/AuthenticationFilter.java
@@ -4,21 +4,25 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.List;
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import me.simplq.exceptions.SQAccessDeniedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 @Component
 @Order(1)
 public class AuthenticationFilter implements Filter {
 
+  private static final String UNAUTHORIZED = "Unauthorized";
+  private static final String INVALID_TOKEN = "Invalid auth token";
   private final GoogleIdTokenVerifier verifier;
   private final LoggedInUserInfo loggedInUserInfo;
 
@@ -34,7 +38,13 @@ public class AuthenticationFilter implements Filter {
   }
 
   private void authenticate(String authHeaderVal) {
+    if (StringUtils.isEmpty(authHeaderVal)) {
+      throw new SQAccessDeniedException(UNAUTHORIZED);
+    }
     var token = authHeaderVal.replaceFirst("^Bearer ", "");
+    if (StringUtils.isEmpty(token)) {
+      throw new SQAccessDeniedException(UNAUTHORIZED);
+    }
     if (token.equals("anonymous")) {
       loggedInUserInfo.setUserId(token);
       return;
@@ -43,12 +53,12 @@ public class AuthenticationFilter implements Filter {
     try {
       idToken = verifier.verify(token);
     } catch (GeneralSecurityException e) {
-      throw new SQAccessDeniedException("Unauthorized");
+      throw new SQAccessDeniedException(UNAUTHORIZED);
     } catch (IOException e) {
-      throw new SQAccessDeniedException("Invalid auth token");
+      throw new SQAccessDeniedException(INVALID_TOKEN);
     }
     if (idToken == null) {
-      throw new SQAccessDeniedException("Invalid auth token");
+      throw new SQAccessDeniedException(INVALID_TOKEN);
     }
     loggedInUserInfo.setUserId(idToken.getPayload().getSubject());
   }
