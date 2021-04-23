c7acb8c4375478a8e91e29f524123b51828c4f80
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-04-24 01:14:03 +0530

    Allow credentials on cross origin requests (#134)

--- simplq/src/main/java/me/simplq/config/WebConfig.java
+++ simplq/src/main/java/me/simplq/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
     return new WebMvcConfigurer(){
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedHeaders("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedHeaders("*").allowedOrigins("*").allowCredentials(true);
       }
     };
   }
