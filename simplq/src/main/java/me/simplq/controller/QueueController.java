debe569ed2349d2cbd16b45b76fee04edc47a404
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-04-23 22:16:56 +0530

    Revert "Add docker support and refactoring (#128)"
    This reverts commit f49487504f6a4a1b5ceabcac56a5147318373f02.

--- simplq/src/main/java/me/simplq/controller/QueueController.java
+++ simplq/src/main/java/me/simplq/controller/QueueController.java
@@ -1,18 +1,6 @@
 package me.simplq.controller;
 
 import javax.validation.Valid;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
 import me.simplq.controller.model.queue.CreateQueueRequest;
 import me.simplq.controller.model.queue.CreateQueueResponse;
@@ -24,6 +12,9 @@ import me.simplq.controller.model.queue.QueueDetailsResponse;
 import me.simplq.controller.model.queue.QueueStatusResponse;
 import me.simplq.controller.model.queue.UpdateQueueStatusResponse;
 import me.simplq.service.QueueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
@@ -78,7 +69,7 @@ public class QueueController {
     } else if (queueName != null) {
       return ResponseEntity.ok(queueService.getQueueStatusByName(queueName));
     } else {
-      return ResponseEntity.badRequest().build(); // Todo Give reason
+      return new ResponseEntity(HttpStatus.BAD_REQUEST); // Todo Give reason
     }
   }
 }
