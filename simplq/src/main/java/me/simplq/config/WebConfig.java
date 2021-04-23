5aefd5726e4b6acd7ed542ad4c7155a9b443e503
Alexandru Chelariu <alex1991.9@gmail.com>
2021-04-23 18:01:59 +0300

    add allow origin

--- simplq/src/main/java/me/simplq/config/WebConfig.java
+++ simplq/src/main/java/me/simplq/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
     return new WebMvcConfigurer(){
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedHeaders("*");
+        registry.addMapping("/**").allowedHeaders("*").allowedOrigins("*");
       }
     };
   }
