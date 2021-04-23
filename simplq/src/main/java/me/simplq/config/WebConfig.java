fbc47808433e4ac0853b8c62479c7ad2ae58902a
Dalton Fury <daltonfury42@gmail.com>
2021-04-24 01:37:54 +0530

    Use FilterRegistrationBean for setting cors with higher presendence

--- simplq/src/main/java/me/simplq/config/WebConfig.java
+++ simplq/src/main/java/me/simplq/config/WebConfig.java
@@ -1,24 +1,28 @@
 package me.simplq.config;
 
+import java.util.Collections;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebConfig {
 
   @Bean
-  public WebMvcConfigurer webMvcConfigurer() {
-    return new WebMvcConfigurer(){
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedHeaders("*")
-            .allowedOrigins("*")
-            .allowedMethods("*")
-            .allowCredentials(true);
-      }
-    };
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
