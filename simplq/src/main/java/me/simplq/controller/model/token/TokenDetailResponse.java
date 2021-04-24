157d7475ebe88f5b2d6a318728d0edfe6371a19b
Dalton Fury <daltonfury42@gmail.com>
2021-04-24 10:20:13 +0530

    expose get token byphone number

--- simplq/src/main/java/me/simplq/controller/model/token/TokenDetailResponse.java
+++ simplq/src/main/java/me/simplq/controller/model/token/TokenDetailResponse.java
@@ -3,6 +3,7 @@ package me.simplq.controller.model.token;
 import java.util.Date;
 import lombok.Data;
 import me.simplq.constants.TokenStatus;
+import me.simplq.dao.Token;
 
 @Data
 public class TokenDetailResponse {
@@ -17,4 +18,18 @@ public class TokenDetailResponse {
   final Long aheadCount;
   final Boolean notifiable;
   final Date tokenCreationTimestamp;
+
+  public static TokenDetailResponse fromEntity(Token token) {
+    return new TokenDetailResponse(
+        token.getTokenId(),
+        token.getName(),
+        token.getContactNumber(),
+        token.getTokenNumber(),
+        token.getStatus(),
+        token.getQueue().getQueueName(),
+        token.getQueue().getQueueId(),
+        token.getAheadCount(),
+        token.getNotifiable(),
+        token.getTokenCreationTimestamp());
+  }
 }
