0d8473b02b8aba0d52106d1a28dd440213115a07
daltonfury42 <daltonfury42@users.noreply.github.com>
2021-10-24 20:15:55 +0530

    Fix bad TokenStatus

--- simplq/src/main/java/me/simplq/dao/Token.java
+++ simplq/src/main/java/me/simplq/dao/Token.java
@@ -69,6 +69,6 @@ public class Token {
   }
 
   public boolean isNotifiable() {
-    return TokenStatus.REMOVED.equals(status) && StringUtils.isNotBlank(emailId);
+    return TokenStatus.WAITING.equals(status) && StringUtils.isNotBlank(emailId);
   }
 }
