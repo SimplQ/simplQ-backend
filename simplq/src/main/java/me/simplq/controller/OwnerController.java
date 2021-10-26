cd37c13f2e693ff4ad39174f4f29c5d7bba1836b
Dalton Fury <daltonfury42@gmail.com>
2021-10-26 20:40:26 +0530

    Respond with device ID on link/unlink calls

--- simplq/src/main/java/me/simplq/controller/OwnerController.java
+++ simplq/src/main/java/me/simplq/controller/OwnerController.java
@@ -1,6 +1,8 @@
 package me.simplq.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.simplq.controller.model.owner.LinkDeviceResponse;
+import me.simplq.controller.model.owner.UnlinkDeviceResponse;
 import me.simplq.service.OwnerService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,14 @@ public class OwnerController {
   private final OwnerService ownerService;
 
   @PutMapping(path = "/link")
-  public void linkDevice(@RequestParam() String deviceId) {
+  public LinkDeviceResponse linkDevice(@RequestParam() String deviceId) {
     ownerService.linkDevice(deviceId);
+    return new LinkDeviceResponse(deviceId);
   }
 
   @PatchMapping(path = "/unlink")
-  public void unlinkDevice(@RequestParam() String deviceId) {
+  public UnlinkDeviceResponse unlinkDevice(@RequestParam() String deviceId) {
     ownerService.unlinkDevice(deviceId);
+    return new UnlinkDeviceResponse(deviceId);
   }
 }
---
+++ simplq/src/main/java/me/simplq/controller/model/owner/LinkDeviceResponse.java
@@ -0,0 +1,8 @@
+package me.simplq.controller.model.owner;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LinkDeviceResponse {
+    final String deviceId;
+}
