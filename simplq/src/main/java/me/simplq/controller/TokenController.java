aadc50b41403e2ac4d5092b431ff01ff39ebbe58
Dalton Fury <daltonfury42@gmail.com>
2022-04-24 23:13:28 +0530

    Add patch queue to move token to another queue

--- simplq/src/main/java/me/simplq/controller/TokenController.java
+++ simplq/src/main/java/me/simplq/controller/TokenController.java
@@ -3,6 +3,7 @@ package me.simplq.controller;
 import lombok.RequiredArgsConstructor;
 import me.simplq.controller.model.token.CreateTokenRequest;
 import me.simplq.controller.model.token.MyTokensResponse;
+import me.simplq.controller.model.token.PatchTokenRequest;
 import me.simplq.controller.model.token.TokenDeleteResponse;
 import me.simplq.controller.model.token.TokenDetailResponse;
 import me.simplq.controller.model.token.TokenNotifyResponse;
@@ -10,6 +11,7 @@ import me.simplq.service.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -56,5 +58,10 @@ public class TokenController {
   @PutMapping(path = "/token/notify/{tokenId}")
   public ResponseEntity<TokenNotifyResponse> notifyToken(@PathVariable("tokenId") String tokenId) {
     return ResponseEntity.ok(tokenService.notifyToken(tokenId));
+  }
+
+  @PatchMapping(path = "/token/{tokenId}")
+  public ResponseEntity<TokenDetailResponse> patchToken(@PathVariable("tokenId") String tokenId, @RequestBody PatchTokenRequest patchTokenRequest) {
+    return ResponseEntity.ok(tokenService.patchToken(tokenId, patchTokenRequest));
   }
 }
---
+++ simplq/src/main/java/me/simplq/controller/model/token/PatchTokenRequest.java
@@ -0,0 +1,8 @@
+package me.simplq.controller.model.token;
+
+import lombok.Data;
+
+@Data
+public class PatchTokenRequest {
+  String queueId;
+}
